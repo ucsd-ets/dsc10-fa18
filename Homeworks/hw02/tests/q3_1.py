a75368628079683ccf6957840413a7a80a40ee91
Aaron Fraenkel <aaron.fraenkel@gmail.com>
2018-10-08 18:19:14 -0700

    hw02

---
+++ Homeworks/hw02/tests/q3_1.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'Question 3_1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> import numpy
+          >>> isinstance(index_of_second, numpy.int64) or isinstance(index_of_second, int)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ Homeworks/hw02/tests/q5_2.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'Question 5_2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Hint: try the diff() function
+          >>> 30 < biggest_increase < 47
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
