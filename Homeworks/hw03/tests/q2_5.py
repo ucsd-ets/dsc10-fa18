089d6235b0023abfb223b986093bd36da6153a1f
mtc017 <32399837+mtc017@users.noreply.github.com>
2018-10-15 17:54:27 -0700

    add hw 3

---
+++ Homeworks/hw03/tests/q2_5.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question 1_1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(above_9, np.ndarray) # your answer should be an array!
+          True
+          >>> isinstance(above_9, Table) # your answer should *not* be a Table!
+          False
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
