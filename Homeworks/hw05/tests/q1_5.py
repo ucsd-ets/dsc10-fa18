888ebeffcf7e0ec9edc8cddc979a3d97b5ef57fe
mtc017 <32399837+mtc017@users.noreply.github.com>
2018-11-06 13:59:17 -0800

    add hw5

---
+++ Homeworks/hw05/tests/q1_5.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question 1_5',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(non_american_aiports, float)
+          True
+          >>> (non_american_aiports >= 0) and (non_american_aiports <= 100) # 0 <= Percentage <= 100
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
