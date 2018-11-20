0c5cffa61b852ad5cc848cd3d9203b2cdc9e49ad
mtc017 <32399837+mtc017@users.noreply.github.com>
2018-11-19 20:23:12 -0800

    add hw 7

---
+++ Homeworks/hw07/tests/q2_10.py
@@ -0,0 +1,26 @@
+test = {
+  'name': 'Question 2_9',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(left_endpoint, float)
+          True
+          >>> isinstance(right_endpoint, float)
+          True
+          >>> left_endpoint <= right_endpoint
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ Homeworks/hw07/tests/q2_6.py
@@ -0,0 +1,26 @@
+test = {
+  'name': 'Question 2_6',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(lower_bound, float)
+          True
+          >>> isinstance(upper_bound, float)
+          True
+          >>> lower_bound <= upper_bound
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
